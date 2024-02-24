b9277a7d85df74a14bc03ae173c9741d437f42e7
Lima404 <gabriel.lima2307@uol.com.br>
2024-02-24 15:28:16 -0300

    Adcionando novos serviços

--- servicos/livro.js
+++ servicos/livro.js
@@ -1,9 +1,41 @@
 const fs = require("fs")
 
 function getTodosLivros() {
-    return JSON.parse( fs.readFileSync("livros.json") )
+    return JSON.parse( fs.readFileSync( "livros.json" ) )
+}
+
+function getLivroId(id) {
+    const livros = JSON.parse (fs.readFileSync( "livros.json" ))
+    const livroFiltrado = livros.filter ( livro => livro.id === id  )[0]
+    return livroFiltrado
+    // [ { id:2, nome: 'Livro irado' } ]
+}
+
+function insereLivro(livroNovo) {
+    const livros = JSON.parse (fs.readFileSync( "livros.json" ))
+
+    const novaListaDeLivros = [ ...livros, livroNovo ]
+    fs.writeFileSync( "livros.json", JSON.stringify( novaListaDeLivros ) )
+}
+
+function modificaLivro(modificacoes, id) {
+    let livrosAtuais = JSON.parse (fs.readFileSync( "livros.json" ))
+    const indiceModificado = livrosAtuais.findIndex( livro => livro.id === id )
+    const conteudoMudado = { ...livrosAtuais[ indiceModificado ], ...modificacoes }
+    livrosAtuais[indiceModificado] = conteudoMudado
+    fs.writeFileSync("livros.json", JSON.stringify(livrosAtuais))
+}
+
+function apagaLivroId(id){
+    const livros = JSON.parse (fs.readFileSync( "livros.json" ))
+    const livrosFiltrados = livros.filter( livro => livro.id !== id )
+    fs.writeFileSync("livros.json", JSON.stringify( livrosFiltrados ))
 }
 
 module.exports = {
-    getTodosLivros
+    getTodosLivros,
+    getLivroId,
+    insereLivro,
+    modificaLivro,
+    apagaLivroId
 }
